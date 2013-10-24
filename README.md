7a0d7be4de3620e9a2a495d2382067c4e21a3c28
Anthony Ruhier <anthony.ruhier@gmail.com>
2013-10-24 19:57:11 +0200

    Add indications for the database configuration

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
-miniprojetbdd
-=============
+Projet de BDD, S3 IUT
+======================
 
-Mini projet bdd
+"Mini" projet consistant à créer un site pour gérer des manifestations
+organisées par plusieurs IUT de France.
+
+
+Configuration :
+---------------
+
+Modifier le fichier 'config.php' pour associer le site à votre base de
+données MySQL.
--- index.php
+++ index.php
@@ -17,7 +17,11 @@ function printWelcome() {
                         <p>Ce site vous permet de gérer facilement les manifestations sportives des IUT de France, via différentes pages offrant une gestion des étudiants inscrits ainsi que des épreuves prévues pour chaque manifestations. <br>
                         La page "Manifestations" affiche les manifestations prévues à partir de la date du jour, "Épreuves" affiche les épreuves programmées pour chaque manifestation et "IUT" liste les IUT inscrits à ce programme.</p>
 
-                        <p>Il s\'agit d\'un projet étudiant de l\'IUT de Belfort-Montbéliard, dans le cadre du module Base de Données, 3ème semestre.
+                        <p>Il s\'agit d\'un projet étudiant de l\'IUT de Belfort-Montbéliard, dans le cadre du module Base de Données, 3ème semestre.</p>
+
+                        <h3>Configuration</h3>
+
+                        <p>Modifier le fichier <b>config.php</b> à la racine du projet pour associer le site à votre base de donnée MySQL.</p>
                     </article>
     ');
 }
